ca4bafdd62d619ec4f46fd309ceb39ccf3a3c86d
boskostankovic <zekave@yahoo.com>
2016-10-07 16:10:34 +0200

    Promenjena StavkaTure

--- src/rmaster/models/StavkaTure.java
+++ src/rmaster/models/StavkaTure.java
@@ -5,7 +5,9 @@
  */
 package rmaster.models;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -14,56 +16,159 @@ import java.util.Map;
  */
 public class StavkaTure {
     
-    public String imeArtikla;
-    public int kolicina;
-    public int cena;
     public String artikalId;
+    public String imeArtikla;
+    public double kolicina = 0;
+    public double cenaJedinicna = 0;
+    public double cena = 0;
+    public List<StavkaTure> dodatniArtikli = new ArrayList(); 
+    public List<StavkaTure> opisniArtikli = new ArrayList(); 
     
-    
+
     public String getArtikalId(){
         return this.artikalId;
     }
-    
-    public void setCena(int novaCena) {
-        this.cena = novaCena;
-    }
-    
+
     public StavkaTure(Map<String, String> stavkaTure) {
         String novaKolicina = stavkaTure.get("kolicina");
         if (novaKolicina.contains("x")) {
             novaKolicina.substring(1);
         }
-        this.imeArtikla = stavkaTure.get("naziv");
-        this.kolicina = Integer.parseInt(novaKolicina);
-        this.cena = Integer.parseInt(stavkaTure.get("cena"));
-        
         this.artikalId = stavkaTure.get("ARTIKAL_ID");
+        this.imeArtikla = stavkaTure.get("naziv");
+        this.kolicina = Double.parseDouble(novaKolicina);
+        //this.cena = Double.parseDouble(stavkaTure.get("cena"));
+        setCenaJedinicna(Double.parseDouble(stavkaTure.get("cenaJedinicna")));
     }
     
     public Map<String, String> dajStavkuTure() {
         Map<String, String> stavkaTure = new HashMap<>();
-//        stavkaTure.put("id", this.stavkaTureId);
+
+        stavkaTure.put("artikalId", this.artikalId);
         stavkaTure.put("naziv", this.imeArtikla);
         stavkaTure.put("kolicina", "x" + this.kolicina);
+        stavkaTure.put("cenaJedinicna", this.cenaJedinicna + "");
         stavkaTure.put("cena", this.cena + "");
-//        stavkaTure.put("artikalId", this.artikalId);
         
         return stavkaTure;
     } 
     
-    public void promeniKolicinu(int novaKolicina) {
+/************************************************************************************/
+/********************* Rad sa opisnim artiklima *************************************/
+/************************************************************************************/
+    public List<StavkaTure> getArtikliOpisni(){
+        return opisniArtikli;
+    }
+    /*** Dodavanje opisnog artikla za kolicinu koja je prosledjena kroz StavkaTure ***/
+    public void addArtikalOpisni(StavkaTure opisniArtikal){
+        for (StavkaTure opArtikal: opisniArtikli) {
+            if (opArtikal.artikalId == opisniArtikal.artikalId) {
+                opArtikal.povecajKolicinuZa(opisniArtikal.kolicina);
+                return;
+            }
+        }
+        opisniArtikli.add(opisniArtikal);
+    }
+    /*** Oduzimanje opisnog artikla 
+     *   za kolicinu koja je prosledjena kroz StavkaTure,
+     *   brisanje ako je nova kolicina = 0 ***/
+    public void removeArtikalOpisni(StavkaTure opisniArtikal){
+        for (StavkaTure opArtikal: opisniArtikli) {
+            if (opArtikal.artikalId == opisniArtikal.artikalId) {
+                opArtikal.smanjiKolicinuZa(opisniArtikal.kolicina);
+                if (opArtikal.kolicina == 0)
+                    opisniArtikli.remove(opArtikal);
+            }
+        }
+    }
+/************************************************************************************/
+/********************* Rad sa opisnim artiklima - KRAJ ******************************/
+/************************************************************************************/
+
+    
+/************************************************************************************/
+/********************* Rad sa dodatnim artiklima ************************************/
+/************************************************************************************/
+    public List<StavkaTure> getArtikliDodatni(){
+        return dodatniArtikli;
+    }
+    /*** Dodavanje dodatnog artikla za kolicinu koja je prosledjena kroz StavkaTure ***/
+    public void addArtikalDodatni(StavkaTure dodatniArtikal){
+        for (StavkaTure dodArtikal: dodatniArtikli) {
+            if (dodArtikal.artikalId == dodatniArtikal.artikalId) {
+                dodArtikal.povecajKolicinuZa(dodatniArtikal.kolicina);
+                return;
+            }
+        }
+        dodatniArtikli.add(dodatniArtikal);
+    }
+    /*** Oduzimanje dodatnog artikla za kolicinu koja je prosledjena kroz StavkaTure, brisanje ako je nova kolicina = 0 ***/
+    public void removeArtikalDodatni(StavkaTure dodatniArtikal){
+        for (StavkaTure dodArtikal: dodatniArtikli) {
+            if (dodArtikal.artikalId == dodatniArtikal.artikalId) {
+                dodArtikal.smanjiKolicinuZa(dodatniArtikal.kolicina);
+                if (dodArtikal.kolicina == 0)
+                    dodatniArtikli.remove(dodArtikal);
+            }
+        }
+    }
+/************************************************************************************/
+/********************* KRAJ - Rad sa dodatnim artiklima *****************************/
+/************************************************************************************/
+
+    
+    
+/************************************************************************************/
+/************************* Rad sa cenama ********************************************/
+/************************************************************************************/
+    public void setCenaJedinicna(double cenaJedinicna) {
+        this.cenaJedinicna = cenaJedinicna;
+        cenaObracunaj();
+    }
+
+    public void cenaObracunaj() {
+        this.cena = this.cenaJedinicna * this.kolicina;
+    }
+/************************************************************************************/
+/************************* Rad sa cenama - KRAJ *************************************/
+/************************************************************************************/
+    
+    
+/************************************************************************************/
+/************************* Rad sa kolicinama ****************************************/
+/************************************************************************************/
+    /*** Promena kolicine ***/
+    public void promeniKolicinu(double novaKolicina) {
             this.kolicina = novaKolicina;
+            this.cenaObracunaj();
     }
     
+    /*** Povecavanje kolicine ***/
     public void povecajKolicinu() {
-            int novaKolicina = this.kolicina;
-            novaKolicina++;
-            this.kolicina = novaKolicina;
+            this.kolicina = this.kolicina + 1;
+            this.cenaObracunaj();
     }
-    
+    public void povecajKolicinuZa(double promena) {
+            this.kolicina = this.kolicina + promena;
+            if (this.kolicina<0)
+                this.kolicina=0;
+            this.cenaObracunaj();
+    }
+
+    /*** Smanjivanje kolicine ***/
     public void smanjiKolicinu() {
-            int novaKolicina = this.kolicina;
-            novaKolicina--;
-            this.kolicina = novaKolicina;
+            this.kolicina = this.kolicina - 1;
+            if (this.kolicina<0)
+                this.kolicina=0;
+            this.cenaObracunaj();
     }
+    public void smanjiKolicinuZa(double smanjiZa) {
+            this.kolicina = this.kolicina - smanjiZa;
+            if (this.kolicina<0)
+                this.kolicina=0;
+            this.cenaObracunaj();
+    }
+/************************************************************************************/
+/************************* Rad sa kolicinama - KRAJ *********************************/
+/************************************************************************************/
 }
