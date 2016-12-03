f4c4c4efbe475d484291d8a5859b648d3ff56bef
Sasa Stojkovic <sasa.stojkovic@arbor-education.com>
2016-12-03 20:43:06 +0100

    5 - Unos lozinke za logovanje preko tastature

--- src/rmaster/views/PocetniEkranController.java
+++ src/rmaster/views/PocetniEkranController.java
@@ -6,39 +6,103 @@
 package rmaster.views;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
 import javafx.animation.Timeline;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.Label;
+import rmaster.assets.FXMLDocumentController;
+import rmaster.assets.ScreenMap;
 /**
  *
  * @author Arbor
  */
-public class PocetniEkranController extends NumberKeypadController {
-    
+public class PocetniEkranController extends FXMLDocumentController {
+   
     @FXML 
     private Label response;
-    
+        
     @FXML
     private PasswordField lozinka;
     
     @FXML
     private Label clock;
-    
-    @FXML
-    private Button test;
-    
-    private final NumberKeypadController keypad = new NumberKeypadController();
-    
+   
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        keypad.sledecaForma = "prikazSala";
+        lozinka.requestFocus();
         Timeline timeline = this.prikaziCasovnik(clock);
         timeline.play();
-
     }
     
+   public void backButton(ActionEvent event) {
+        response.setText("");
+        String lozinkaText = this.lozinka.getText();
+        
+        if (lozinkaText.length() != 0) {
+            lozinkaText = lozinkaText.substring(0, lozinkaText.length()-1);
+            this.lozinka.setText(lozinkaText);
+        }
+    }
    
+    public void numberKeyPressed(ActionEvent event) throws Exception {
+        
+        Button pritisnutTaster = (Button)event.getSource();
+        
+        String lozinkaText = this.lozinka.getText();
+        
+        if (lozinkaText.length() < 4) {
+           response.setText(""); 
+           lozinkaText += pritisnutTaster.getText();
+           this.lozinka.setText(lozinkaText);
+        } 
+        
+    }  
+    
+    public void passwordCheck(ActionEvent event) throws Exception {
+        response.setText("");
+        String lozinkaText = this.lozinka.getText();
+        
+        if (lozinkaText.equals("9988")) {
+            Platform.exit();
+            System.exit(0);        
+        }
+        
+        String[] uslovneKolone = {"pin"};
+        String[] uslovneVrednosti = {lozinkaText};
+        
+        List rezultat = vratiKoloneIzTabele(
+                "konobar", 
+                uslovneKolone, 
+                uslovneVrednosti
+        );
+        
+        if (!rezultat.isEmpty()) {
+            Map<String,String>  konobar = (Map<String, String>)rezultat.get(0);
+            
+            //zapamti KonobarID
+            setUlogovaniKonobar(konobar);
+            
+            Map<String, String> newData = new HashMap<>();
+                            
+            //sledeca stranica 
+            prikaziFormu(
+                    newData,
+                    ScreenMap.PRIKAZ_SALA, 
+                    true, 
+                    response
+            );
+            return;
+        }
+        
+        response.setText("Neuspelo logovanje!");
+        lozinka.setText("");
+        lozinka.requestFocus();
+    }
 }
